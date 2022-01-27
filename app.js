8a3b2c650a89d6453463eacda5ab20a431c8d6bb
Luís Henrique Braga de Sousa <agarb@pop-os.localdomain>
2022-01-27 13:30:40 -0300

    Feat: Add validations and data save of body requests on post in /participants route

--- app.js
+++ app.js
@@ -1,17 +1,76 @@
 import express from "express"
 import cors from "cors"
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv"
+import dayjs from 'dayjs';
 
+dotenv.config();
+dayjs().format()
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 
-server.post("/participants", (request, response) => {
-    response.send("ok");
+server.post("/participants", async (request, response) => {
+    let mongoClient
+
+    try{
+        mongoClient = new MongoClient(process.env.MONGO_URI)
+        await mongoClient.connect();
+
+        const example = mongoClient.db("exemplo");
+        const contactsCollection = example.collection('contatos');
+
+        const participantName = request.body.name;
+    
+        if(participantName === ''){
+            response.status(422).send("Todos os campos são obrigatórios!")
+            return;
+        }
+        
+        const contacts = await contactsCollection.find({}).toArray();
+        const participantNameInUse = contacts.find(person => person.name === participantName)
+    
+        if(participantNameInUse){
+            response.status(409).send("Nome em uso")
+            return;
+        }
+    
+        await contactsCollection.insertMany([
+            {...request.body, lastStatus: Date.now()},
+            {from: request.body.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}'`} 
+        ]);
+
+        response.sendStatus(201);
+        mongoClient.close()
+
+    }catch{
+        response.sendStatus(500)
+        mongoClient.close();
+    }
 })
 
-server.get("/participants", (request, response) => {
-    response.send("ok");
+server.get("/participants", async (request, response) => {
+    let mongoClient
+
+    try{
+        mongoClient = new MongoClient(process.env.MONGO_URI)
+        await mongoClient.connect();
+    
+        const example = mongoClient.db("exemplo");
+        const contactsCollection = example.collection('contatos');
+        const contacts = await contactsCollection.find({}).toArray();
+    
+    
+        response.send(contacts);
+        mongoClient.close();
+
+    } catch {
+
+        response.sendStatus(500)
+        mongoClient.close();
+    }
+
 })
 
 server.post("/messages", (request, response) => {
@@ -27,4 +86,4 @@ server.post("/status", (request, response) => {
 })
 
 
-server.listen(4000, () => console.log("Servidor iniciado em http://localhost:4000"));
+server.listen(5000, () => console.log("Servidor iniciado em http://localhost:5000"));
